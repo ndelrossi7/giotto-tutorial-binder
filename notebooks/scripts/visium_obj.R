90d9ef950ff039402c7b7b0fcce748f31dfd18fe
ndelrossi7 <natalie.delrossi@mail.mcgill.ca>
2021-03-18 11:30:35 -0400

    update vis script

--- notebooks/scripts/visium_obj.R
+++ notebooks/scripts/visium_obj.R
@@ -21,3 +21,25 @@ my_vis_object <- calculateHVG(gobject = my_vis_object, show_plot = FALSE)
 
 # PCA
 my_vis_object <- runPCA(gobject = my_vis_object)
+
+# create nearest network
+my_vis_object <- createNearestNetwork(
+  my_vis_object,
+  type = "sNN", 
+  dim_reduction_to_use = "pca",
+  dim_reduction_name = "pca",
+  dimensions_to_use = 1:10,
+  genes_to_use = NULL,
+  expression_values = c("normalized", "scaled", "custom"),
+  name = "sNN.pca",
+  return_gobject = TRUE,
+  k = 30,
+  minimum_shared = 5,
+  top_shared = 3,
+  verbose = T,
+)
+
+# Leiden clustering
+my_vis_object <- doLeidenCluster(
+  my_vis_object, 
+  name = 'leiden_clus')
