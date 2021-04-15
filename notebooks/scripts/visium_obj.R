cd98922fcbed64472e06811d283171f2c592e7b0
ndelrossi7 <natalie.delrossi@mail.mcgill.ca>
2021-04-15 12:03:57 -0400

    update vis preprocess

--- notebooks/scripts/visium_obj.R
+++ notebooks/scripts/visium_obj.R
@@ -10,43 +10,86 @@ my_instructions = createGiottoInstructions(python_path = "/srv/conda/envs/notebo
 library(data.table)
 # install quadprog - necessary for DWLS package
 
-# path to data
-visium_path = system.file("extdata", "visium_DG_expr.txt", package = "Giotto")
-visium_locations = system.file("extdata", "visium_DG_locs.txt", package = "Giotto")
+# paths to expression and location data
+expr_path = system.file("extdata", "visium_DG_expr.txt", package = 'Giotto')
+loc_path = system.file("extdata", "visium_DG_locs.txt", package = 'Giotto')
 
-# creating the Giotto object
-my_vis_object = createGiottoObject(raw_exprs = visium_path,
-                                      spatial_locs = visium_locations,
-                                      instructions = my_instructions)
+my_visium_object <- createGiottoObject(raw_exprs = expr_path,
+                                       spatial_locs = loc_path,
+                                       instructions = my_instructions
+                                      )
 
-# normalize object
-my_vis_object <- normalizeGiotto(gobject = my_vis_object)
+# read image
+png_path = system.file("extdata", "deg_image.png", package = 'Giotto')
+mg_img = magick::image_read(png_path)
 
-# calculate HVGs
-my_vis_object <- calculateHVG(gobject = my_vis_object, show_plot = FALSE)
+# test and modify image alignment
+mypl = spatPlot(my_visium_object, return_plot = T, point_alpha = 0.8, show_plot = FALSE)
+orig_png = createGiottoImage(gobject = my_visium_object, mg_object = mg_img, name = 'image',
+                             xmax_adj = 450, xmin_adj = 550,
+                             ymax_adj = 200, ymin_adj = 200)
+mypl_image = addGiottoImageToSpatPlot(mypl, orig_png)
 
-# PCA
-my_vis_object <- runPCA(gobject = my_vis_object)
+
+# add image to Giotto object
+
+image_list = list(orig_png)
+my_visium_object = addGiottoImage(gobject = my_visium_object,
+                             images = image_list)
+
+
+# explore gene and cell distribution
+filterDistributions(my_visium_object, detection = 'genes', 
+                    return_plot = FALSE, 
+                    show_plot = FALSE)
+filterDistributions(my_visium_object, detection = 'cells', 
+                    return_plot = FALSE, 
+                    show_plot = FALSE)
+filterCombinations(my_visium_object,
+                   expression_thresholds = c(1),
+                   gene_det_in_min_cells = c(20, 20, 50, 50),
+                   min_det_genes_per_cell = c(100, 200, 100, 200), 
+                   return_plot = FALSE, 
+                   show_plot = FALSE)
+
+# filter and normalize
+my_visium_object <- filterGiotto(gobject = my_visium_object,
+                            expression_threshold = 1,
+                            gene_det_in_min_cells = 50,
+                            min_det_genes_per_cell = 100,
+                            expression_values = c('raw'),
+                            verbose = T)
+my_visium_object <- normalizeGiotto(gobject = my_visium_object, scalefactor = 6000, verbose = T)
+my_visium_object <- addStatistics(gobject = my_visium_object)
+
+# calculate highly variable genes
+my_visium_object <- calculateHVG(gobject = my_visium_object, 
+                                 return_plot = FALSE,
+                                 show_plot = FALSE)
+
+# run PCA
+my_visium_object <- runPCA(gobject = my_visium_object)
+
+# run UMAP
+my_visium_object <- runUMAP(my_visium_object, dimensions_to_use = 1:10)
+
+# run tSNE
+my_visium_object <- runtSNE(my_visium_object, dimensions_to_use = 1:10)
 
 # create nearest network
-my_vis_object <- createNearestNetwork(
-  my_vis_object,
-  type = "sNN", 
-  dim_reduction_to_use = "pca",
-  dim_reduction_name = "pca",
-  dimensions_to_use = 1:10,
-  genes_to_use = NULL,
-  expression_values = c("normalized", "scaled", "custom"),
-  name = "sNN.pca",
-  return_gobject = TRUE,
-  k = 30,
-  minimum_shared = 5,
-  top_shared = 3,
-  verbose = T,
-)
+my_visium_object <- createNearestNetwork(gobject = my_visium_object, dimensions_to_use = 1:10, k = 20)
+# leiden clustering
+my_visium_object <- doLeidenCluster(gobject = my_visium_object, resolution = 0.4, n_iterations = 1000)
 
-# Leiden clustering
-my_vis_object <- doLeidenCluster(
-  my_vis_object, 
-  name = 'leiden_clus')
+# find SCRAN markers
+scran_markers = findMarkers_one_vs_all(gobject = my_visium_object,
+                                       method = 'scran',
+                                       expression_values = 'normalized',
+                                       cluster_column = 'leiden_clus')
+
+# annotate
+cluster_cell_types = c('Gfap_cells', 'Tbr1_cells', 'Tcf7l2_cells', 'Wfs1_cells', 'Nptxr_cells')
+names(cluster_cell_types) = 1:5
+my_visium_object = annotateGiotto(gobject = my_visium_object, annotation_vector = cluster_cell_types,
+                             cluster_column = 'leiden_clus', name = 'cluster_cell_types')
 
